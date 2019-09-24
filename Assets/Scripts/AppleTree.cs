c95ffe7391c0cbfd4c1376fcf3b2510cde3852eb
Christopher Wind <christopherwind843@gmail.com>
2019-09-24 16:11:59 -0400

    adds drop apple function to appletree script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -13,28 +13,36 @@ public class AppleTree : MonoBehaviour
     
     void Start(){
     // Dropping apples every second 
-        
+        Invoke("DropApple",2f); //Invoke() function calls a named function in a certain number of seconds
+        //in this instance, it calls function DropApple() but tells Invoke() to wait 2 seconds before it does so
     }
 
+    void DropApple(){ //custom function to instantiate an Apple prefab object at the AppleTree's location
+        GameObject apple= Instantiate<GameObject>(applePrefab);// creates an instance of applePrefab and assigns it to GameObject variable apple
+        apple.transform.position= transform.position; //sets apple variable position to appletree's current location
+        Invoke("DropApple",secondsBetweenAppleDrops); //calls invoke() again, but will be executed in secondsBetweenAppleDrops seconds, which is set to 1
+    }
     
     void Update() {
 
-    // Basic Movement
-    Vector3 pos= transform.position; //defines the Vector3 pos to be current position of Apple Tree
-    pos.x = pos.x+ (speed*Time.deltaTime); //increases x pos value by speed times deltaTime
-    //deltaTime is a measure of the seconds since the last frame. makes the movement of the appleTree TIME BASED
-    transform.position= pos; //assigns this modified pos back to transform.position (which moves AppleTree to a new position)
+        // Basic Movement
+        Vector3 pos= transform.position; //defines the Vector3 pos to be current position of Apple Tree
+        pos.x = pos.x+ (speed*Time.deltaTime); //increases x pos value by speed times deltaTime
+        //deltaTime is a measure of the seconds since the last frame. makes the movement of the appleTree TIME BASED
+        transform.position= pos; //assigns this modified pos back to transform.position (which moves AppleTree to a new position)
 
-    //Changing Directions    
-    if(pos.x < -leftAndRightEdge){ //if tree is at left edge
-        speed=Mathf.Abs(speed); //move right. sets speed to absolute value of speed, guaranteeing it will be positive
-        //positive speed translates into movement to the right
-    }else if (pos.x > leftAndRightEdge){ //if tree is at right edge
-        speed=-Mathf.Abs(speed); //move left 
-    }
-    else if(Random.value<chanceToChangeDirections){ //
-        speed= speed*-1; //change directions
+        //Changing Directions    
+        if(pos.x < -leftAndRightEdge){ //if tree is at left edge
+            speed=Mathf.Abs(speed); //move right. sets speed to absolute value of speed, guaranteeing it will be positive
+            //positive speed translates into movement to the right
+        }else if (pos.x > leftAndRightEdge){ //if tree is at right edge
+            speed=-Mathf.Abs(speed); //move left 
+        }
     }
 
+    void FixedUpdate(){ //changing direction randomly is now time-based b/c of fixed update
+        if(Random.value<chanceToChangeDirections){ //if random float is less than desired chance value
+            speed= speed*-1; //change directions
+        }
     }
 }
