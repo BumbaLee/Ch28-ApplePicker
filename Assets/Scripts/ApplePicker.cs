b06c12a437e68bc2cdd9b8e2f1eca1273611558b
Christopher Wind <christopherwind843@gmail.com>
2019-09-24 17:04:07 -0400

    adds Basket script's update() function

---
+++ Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker : MonoBehaviour
+{
+    public GameObject basketPrefab;
+    public int numBaskets= 3;
+    public float basketBottomY= -14f;
+    public float basketSpacingY= 2f;
+
+    // Start is called before the first frame update
+    void Start(){ //instantiates three copies of the Basket prefab that are spaced out vertically 
+        for(int i=0; i<numBaskets; i++){
+            GameObject tBasketGo= Instantiate<GameObject>(basketPrefab); 
+            Vector3 pos= Vector3.zero; //create new Vector3 variable named pos, set it to (0,0,0)
+            pos.y= basketBottomY+(basketSpacingY*i);//change pos variable's vector y value 
+            tBasketGo.transform.position= pos; //sets created basket object's position to pos 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos2D= Input.mousePosition; //gets the current screen position of the mouse from Input
+        mousePos2D.z= -Camera.main.transform.position.z; //camera's z position sets how far to push the mouse into 3D
+
+        Vector3 mousePos3D= Camera.main.ScreenToWorldPoint(mousePos2D); //convert the point from 2D screen space into 3D game world space
+
+        //move the x position of this Basket to the x position of the Mouse 
+        Vector3 pos= this.transform.position;
+        pos.x = mousePos3D.x; 
+        this.transform.position= pos; 
+    }
+}
